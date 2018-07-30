29828a23af6c7f4839e83f1215e2ad84768230fb
CurlyCuteLauren <lnziegele@gmail.com>
2018-07-30 16:32:03 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,11 +5,11 @@ class Person
   def name 
     @name 
   end 
-  puts name_one = name.new("Beyonce")
   def job= (job)
     @job = job 
   end 
   def job
     @job 
   end
-end
+end 
+puts name_one = name.new("Beyonce")
